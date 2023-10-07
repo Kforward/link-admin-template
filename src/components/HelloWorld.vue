b06183336ee2e30dcd4b80a7e88b705f614b2497
WanLinkun <15884738915@163.com>
2023-10-07 14:50:59 +0800

    ci(集成 Tailwind CSS): 🎡 集成 Tailwind CSS
    集成 Tailwind CSS

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@ const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="text-3xl font-bold underline">{{ msg }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -27,7 +27,7 @@ const count = ref(0);
     <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="read-the-docs hover:bg-violet-600 active:bg-violet-700">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
