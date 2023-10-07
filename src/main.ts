f26d97594f976fe318dd37bed41514b0172ff123
WanLinkun <15884738915@163.com>
2023-10-07 15:49:29 +0800

    refactor(style): ♻️ 修改文件结构
    修改文件结构，将style.scss 文件移动至styles文件下

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,1 @@
-import { createApp } from "vue";import "./style.css";import App from "./App.vue";createApp(App).mount("#app");
+import { createApp } from "vue";import "./styles/style.css";import App from "./App.vue";createApp(App).mount("#app");
