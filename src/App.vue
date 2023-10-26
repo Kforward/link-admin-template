edb6bbf60492357a268a940ddea9452be63bc5ee
WanLinkun <15884738915@163.com>
2023-10-26 17:45:44 +0800

    feat: 添加threejs页面

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import ThreeJS from "@/views/threeJS/index.vue";
 </script>
 
 <template>
@@ -11,7 +12,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
       <img src="./assets/icons/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <ThreeJS />
 </template>
 
 <style scoped>
---
+++ src/views/threeJS/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div ref="threeContainerRef" class="three-container">empty</div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { World } from "./World/World.js";
+
+const threeContainerRef = ref("");
+
+onMounted(() => {
+  const world = new World(threeContainerRef.value);
+  world.start();
+});
+</script>
+
+<style scoped>
+.three-container {
+  width: 600px;
+  height: 300px;
+}
+</style>
