3d053db9fefec1fc3653cb0ddef44735c258b674
k <67428794+Kforward@users.noreply.github.com>
2024-05-30 13:17:43 +0800

    fix: 添加首页页面

---
+++ src/views/home/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+function goToFt() {
+  window.open("http://192.168.50.228:9527/#/ssoPage?code=123456");
+}
+</script>
+
+<template>
+  <div class="home">
+    <div class="bottom-10">home</div>
+
+    <el-button @click="goToFt">go</el-button>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
